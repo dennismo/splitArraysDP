a616b3c3a8c491fd79746aed0da843546aeea73f
Dennis Mo <dennismoyunfei@gmail.com>
2019-09-05 16:04:56 -0700

    Initial commit

---
+++ splitArraysDP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  splitArraysDP
+//
+//  Created by Dennis Mo on 9/5/19.
+//  Copyright © 2019 Dennis Mo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
