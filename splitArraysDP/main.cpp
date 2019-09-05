b696b24e54b6de80d9b3851dcebe36e0296d3d56
Dennis Mo <dennismoyunfei@gmail.com>
2019-09-05 16:06:00 -0700

    Initialize Files

--- splitArraysDP/main.cpp
+++ splitArraysDP/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
+vector<vector<int>> DP;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
     return 0;
 }
